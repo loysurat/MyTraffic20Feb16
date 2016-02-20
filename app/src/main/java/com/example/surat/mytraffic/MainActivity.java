cc7981b5c3408c3560e8ca5023975cf30fbf5569
surat <surat42@hotmail.com>
2016-02-20 15:08:53 +0700

    finish 20Feb16

--- app/src/main/java/com/example/surat/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/surat/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.surat.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -18,16 +22,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         //Bind Widget
-
         bindWidget();
+
+        //Button Controller
+        buttonController();
 
     } //Main method
 
-    private void bindWidget() {
+    private void buttonController() {
 
+     aboutMeButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+
+             //sound effect
+             MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.horse);
+             mediaPlayer.start();
+
+             //Web view
+
+             Intent intent = new Intent(Intent.ACTION_VIEW);
+             intent.setData(Uri.parse("https://youtu.be/_MuyDzUo8x0"));
+             startActivity(intent);
+
+         }//event
+     });
+
+    } //ButtonController
+
+    private void bindWidget() {
 
         trafficListView = (ListView) findViewById(R.id.listView);
         aboutMeButton = (Button) findViewById(R.id.button);
