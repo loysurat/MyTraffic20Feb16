7bca0476b68aa6264e94f1741433c4d6efce371a
surat <surat42@hotmail.com>
2016-02-20 14:04:04 +0700

    Explicit เสร็จแว้วววว

--- app/src/main/java/com/example/surat/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/surat/mytraffic/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.surat.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    } //Main method
+
+
+} //Main class
